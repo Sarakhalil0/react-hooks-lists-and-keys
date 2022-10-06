a687be2fa4d9ed7b6dd48b4ab2eba52f708d8b4b
sara khalil <sarakhalil3377@gmail.com>
2022-10-06 12:49:26 +0300

    DONE

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,18 +7,25 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "red",
   ];
+
+  const colorsList =colors.map((color)=> <li key={color} style={{color:color}}>{color}</li>)
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
+      {colorsList}
+      </ol>
+
+      {/*<ol>
         <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+  </ol>*/}
     </div>
   );
 }
